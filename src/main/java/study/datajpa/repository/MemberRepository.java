98894d85810306dd5b46116e8ea75a2f03fe8e3c
qkrxodud00 <qkrxodud00@gmail.com>
2022-05-02 18:41:57 +0900

    스프링 데이터 JPA 리포지토리

---
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.datajpa.Entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
+
