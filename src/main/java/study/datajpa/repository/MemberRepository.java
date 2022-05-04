f4e421f23baefcbe2e310f0395f926f47ad2565c
qkrxodud00 <qkrxodud00@gmail.com>
2022-05-04 15:51:03 +0900

    *리포지토리 메소드 쿼리 정의

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,16 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.Entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     public List<Member> findByUserNameAndAgeGreaterThan(String userName, int age);
+
+    @Query("select m from Member m where m.userName = :userName and m.age = :age")
+    List<Member> findUser(@Param("userName") String userName, @Param("age") int age);
 }
 
