10abfee3439bc31e9a11005feb708ed6fd7328b9
qkrxodud00 <qkrxodud00@gmail.com>
2022-05-04 16:14:57 +0900

    *Query, 값, DTO 조회하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.Entity.Member;
+import study.datajpa.dto.MemberDto;
 
 import java.util.List;
 
@@ -12,5 +13,12 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.userName = :userName and m.age = :age")
     List<Member> findUser(@Param("userName") String userName, @Param("age") int age);
+
+    @Query("select m.userName from Member m")
+    List<String> findUserNameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.userName, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
+
 
