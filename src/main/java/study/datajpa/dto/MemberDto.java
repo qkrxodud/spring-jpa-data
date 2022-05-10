bb5d129ca3457c2b5413dca49b4a88370de99e30
qkrxodud00 <qkrxodud00@gmail.com>
2022-05-10 21:35:30 +0900

    *변수명 변경

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,16 +1,21 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.Entity.Member;
 
 @Data
 public class MemberDto {
     private Long id;
     private String userName;
-    private String TeamName;
+    private String teamName;
 
     public MemberDto(Long id, String userName, String teamName) {
         this.id = id;
         this.userName = userName;
-        TeamName = teamName;
+        this.teamName = teamName;
+    }
+    public MemberDto(Member m) {
+        id = m.getId();
+        userName = m.getUserName();
     }
 }
