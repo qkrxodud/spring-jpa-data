ee4e73ffde266874dcee9414ad621d6bbe555e92
qkrxodud00 <qkrxodud00@gmail.com>
2022-05-11 16:35:56 +0900

    *Persistable 구현

---
+++ src/main/java/study/datajpa/Entity/Item.java
@@ -0,0 +1,36 @@
+package study.datajpa.Entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+
+    @Id @GeneratedValue
+    private String id;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @Override
+    public boolean isNew() {
+        return createDate == null;
+    }
+}
